4a29a45454c7df57ed6243d3dc19361a7a094685
ratinoAG14 <ratino@astragraphia.co.id>
2025-03-11 16:37:13 +0700

    <feat> Create sumByKanwil excel in class SummaryModel

--- src/maybank_rk_pdf/model/SummaryModel.java
+++ src/maybank_rk_pdf/model/SummaryModel.java
@@ -5,7 +5,21 @@
  */
 package maybank_rk_pdf.model;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  *
@@ -13,7 +27,54 @@ import java.sql.Statement;
  */
 public class SummaryModel {
     
-    public void createSumByKanwil(Statement stmt){
+    public void createSumByKanwil(String path, Statement stmt){
+        try {
+            ResultSet hasilQuery = null;
+            hasilQuery = stmt.executeQuery("SELECT s1 AS KANWIL, COUTN(DISTInct id_customer) AS NASABAH, COUNT(seq_page) AS HALAMAN, COUNT(s6) AMPLOP FROM t_log GROUP BY s1, ORDER BY s1");
+            
+            Workbook workbook = new XSSFWorkbook();
+            org.apache.poi.ss.usermodel.Sheet sheet = workbook.createSheet("Summary Data");
+            Row headerRow = sheet.createRow(0);
+            String[] columns = {"KANWIL", "NASABAH", "HALAMAN", "AMPLOP"};
+            for(int i = 0; i < columns.length; i++){
+                Cell cell = headerRow.createCell(i);
+                cell.setCellValue(columns[i]);
+                cell.setCellStyle(getHeaderStyle(workbook));
+            }
+            
+            //Write
+            int rowNum = 1;
+            while(hasilQuery.next()){
+                Row row = sheet.createRow(rowNum++);
+                row.createCell(0).setCellValue(hasilQuery.getString("KANWIL"));
+                row.createCell(1).setCellValue(hasilQuery.getString("NASABAH"));
+                row.createCell(2).setCellValue(hasilQuery.getString("HALAMAN"));
+                row.createCell(3).setCellValue(hasilQuery.getString("AMPLOP"));
+            }
+            
+            for(int i = 0; i < columns.length; i++){
+                sheet.autoSizeColumn(i);
+            }
+            
+            try(FileOutputStream fos = new FileOutputStream(path + "Sum_by_Kanwil.xls")){
+                workbook.write(fos);
+            } catch (IOException ex) {
+                Logger.getLogger(SummaryModel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("Create Excel berhasil!!");          
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(SummaryModel.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
+    }
+    
+    private static CellStyle getHeaderStyle(Workbook workbook){
+        CellStyle style = workbook.createCellStyle();
+        Font font = workbook.createFont();
+        font.setBold(true);
+        style.setFont(font);
+        style.setAlignment(HorizontalAlignment.CENTER);
+        return style;        
     }
 }

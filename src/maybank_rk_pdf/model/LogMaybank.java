3c517179801552baaa5c90f886f2da8820b1bf12
ratinoAG14 <ratino@astragraphia.co.id>
2025-03-19 14:47:36 +0700

    <feat> add logMaybank model

---
+++ src/maybank_rk_pdf/model/LogMaybank.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maybank_rk_pdf.model;
+
+import java.sql.Statement;
+
+/**
+ *
+ * @author Ratino
+ */
+public class LogMaybank {
+    
+    
+    public void createLogMaybank(Statement stmt){
+        
+    }
+
+}

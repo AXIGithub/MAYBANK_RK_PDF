eedde2b7ae36aa160e608146fc6926e70b516d6a
Ratino <ratinoray@gmail.com>
2025-03-20 18:18:52 +0700

    <fix> log maybank all add order by courier_name

--- src/maybank_rk_pdf/model/LogMaybank.java
+++ src/maybank_rk_pdf/model/LogMaybank.java
@@ -43,7 +43,7 @@ public class LogMaybank {
             
             ResultSet hasilQuery = null;
             // writeln(logFile            ,padLStr(copy(kanwil,1,2),2,'0')+padRStr(copy(cabang,1,40),40,' ')+ copy(policy,2,3) +padRStr(barcode,28,' ')+padRStr(CIF,16,' ')+padRStr(policy,48,' ')+padRStr(copy(nama,1,30),30,' ')+padRStr(copy(addr1,1,30),30,' ')+padRStr(copy(addr2,1,30),30,' ')+padRStr(copy(addr3,1,30),30,' ')+padRStr(copy(addr4,1,30),30,' ')+padRStr(uppercase(kota),28,' ')+padRStr(Zipcode,234,' ')+padRStr(tanggalCPDP,10,' ')+padRStr(copy(GRUPkurir,1,5),4,' ')+padLStr(inttostr(jumpage),5,' ')+padLStr(inttostr(jumAMPall),5,' ')+padRStr(kurir,5,' ')+padRStr(pickupDate,41,' ')+'.');
-            hasilQuery = stmt.executeQuery("SELECT s1 AS kanwil, name2 AS kd_cabang, id_customer, barcode, name3 AS cif, name1, address1, address2, address3, address4, address5, courier_name, ss1 FROM t_log");
+            hasilQuery = stmt.executeQuery("SELECT s1 AS kanwil, name2 AS kd_cabang, id_customer, barcode, name3 AS cif, name1, address1, address2, address3, address4, address5, address6, courier_name, ss1 FROM t_log GROUP BY id_customer ORDER BY courier_name");
             
             BufferedWriter bw = new BufferedWriter(new FileWriter(fileOut));
             
@@ -65,11 +65,11 @@ public class LogMaybank {
                         text.padRStr(hasilQuery.getString("address6"), 234, ' ') +  // padRStr(Zipcode,234,' ')+
                         text.padRStr(dateNow,10, ' ') +                             // padRStr(tanggalCPDP,10,' ')+
                         text.padRStr("MAIL",4,' ') +     // padRStr(copy(GRUPkurir,1,5),4,' ')+
-                        text.padRStr(hasilQuery.getString("ss1"), 5, ' ') +  // padLStr(inttostr(jumpage),5,' ')+
-                        text.padRStr("0",5,' ') +   // padLStr(inttostr(jumAMPall),5,' ')+
+                        text.padLStr(hasilQuery.getString("ss1"), 5, ' ') +  // padLStr(inttostr(jumpage),5,' ')+
+                        text.padLStr("0",5,' ') +   // padLStr(inttostr(jumAMPall),5,' ')+
                         text.padRStr(hasilQuery.getString("courier_name"), 5, ' ') +  // padRStr(kurir,5,' ')+      
                         text.padRStr(nextDayFormat,41,' ') +        // padRStr(pickupDate,41,' ')+'.');
-                                "\r\n"                        
+                                ".\r\n"                        
                 );
             }
             
